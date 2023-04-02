7db896679cb5158d226bd2456e67a16add7d4afa
arjunr2226 <2226@tkmce.ac.in>
2023-04-02 12:39:41 +0530

    added co3

--- CO1/5/Main.java
+++ CO1/5/Main.java
@@ -2,6 +2,31 @@ import java.util.Scanner;
 
 class CPU
 {
+<<<<<<< HEAD
+   int price = 12;
+
+  class Processor
+  {
+     int noCores = 5;
+     String manufacture = "DELL";
+
+    static class RAM
+    {
+       int memory = 8;
+       String manufacture = "crucial";
+       void show()
+       {
+        CPU cpuObj = new CPU();
+        CPU.Processor processorObj = cpuObj.new Processor();
+        System.out.println("Price: " + cpuObj.price);
+        System.out.println("Number of Cores: " + processorObj.noCores);
+        System.out.println("Processor Manufacture: " + processorObj.manufacture);
+        System.out.println("RAM Memory: " + memory);
+        System.out.println("RAM Manufacture: " + manufacture);
+       }
+    }
+  }
+=======
    int price;
 
   class Processor
@@ -48,13 +73,21 @@ class CPU
         "\nRAM Manufacture: "+manufacture);
        }
     }
+>>>>>>> ac484d427247f220a87dd75d65325d08eb518dc6
 }
 
 public class Main
 {
   public static void main(String args[])
   {
+<<<<<<< HEAD
+    CPU.Processor.RAM o3 = new CPU.Processor.RAM();
+    o3.show();
+  }
+}
+=======
     CPU.RAM o3 = new CPU.RAM();
     o3.show();
   }
 }
+>>>>>>> ac484d427247f220a87dd75d65325d08eb518dc6
--- CO2/3/Main.java
+++ CO2/3/Main.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
+
 public class Main
 {
   public static void main(String args[])
   {
-    String str = "arjun";
+    Scanner sc = new Scanner(System.in);
+    String str;
+    System.out.println("ENTER STRING: ");
+    str = sc.next();
     System.out.println(str.toUpperCase());
     System.out.println(str.toLowerCase());
     System.out.println(str.concat("str"));
@@ -11,7 +16,7 @@ public class Main
     String[] str1 = str.split("r", -2);
     for(String s: str1)
       System.out.println(s);
-
     System.out.println(str.substring(1,5));
+    sc.close();
   }
 }
