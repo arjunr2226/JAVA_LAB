d813a0cfe349837fcbf5c99ad9445b787dcdeafa
arjunr2226 <2226@tkmce.ac.in>
2023-08-01 15:09:32 +0530

    Completed co6

--- CO6/q4/Main.java
+++ CO6/q4/Main.java
@@ -1,28 +1,32 @@
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        try {
-            FileInputStream fin = new FileInputStream("data.txt");
-            FileOutputStream fodd = new FileOutputStream("dataodd.txt");
-            FileOutputStream feven = new FileOutputStream("dataeven.txt");
 
-            int i;
-            while ((i = fin.read()) != -1) {
-                if (i % 2 == 0)
-                    feven.write(i);
-                else
-                    fodd.write(i);
-            }
+	public static void main(String[] args) {
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
+			BufferedWriter writerOdd = new BufferedWriter(new FileWriter("odd.txt"));
+			BufferedWriter writerEven = new BufferedWriter(new FileWriter("even.txt"));
+			
+			String line;
+			while((line=reader.readLine()) != null) {
+				Integer num = Integer.parseInt(line);
+				if(num %2 ==0) {
+					writerEven.write(String.valueOf(num) + "\n");
+				}
+				else {
+					writerOdd.write(String.valueOf(num)+ "\n");
+				}
+			}
+			
+			writerOdd.close();
+			writerEven.close();
+			reader.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
 
-            fodd.close();
-            fin.close();
-            feven.close();
-            System.out.println("Numbers separated successfully.");
-        } catch (IOException e) {
-            System.out.println("An error occurred while separating the numbers: " + e.getMessage());
-        }
-    }
 }
